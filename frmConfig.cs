da377ee10a2ba67e25c4880da42ce438aec1d5a2
jappabr <guilherme.dion@gmail.com>
2023-08-07 19:40:46 -0300

    Update frmConfig.cs

--- frmConfig.cs
+++ frmConfig.cs
@@ -12,9 +12,42 @@ namespace off
 {
     public partial class frmConfig : Form
     {
+        private string selectedFolderPath = "";
+
         public frmConfig()
         {
             InitializeComponent();
+
+            // Exibe o último dado cadastrado no TextBox1, se houver
+            string lastSelectedFolderPath = db.GetSelectedFolderPath();
+            if (!string.IsNullOrEmpty(lastSelectedFolderPath))
+            {
+                textBox1.Text = lastSelectedFolderPath;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "Selecionar Pasta";
+
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedFolderPath = folderBrowserDialog.SelectedPath;
+
+                    // Exibir o caminho da pasta selecionada no TextBox
+                    textBox1.Text = selectedFolderPath;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Salvar a string do TextBox1 no banco de dados
+            db.UpdateSelectedFolderPath(textBox1.Text);
+
+            MessageBox.Show("Caminho da pasta salvo com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
