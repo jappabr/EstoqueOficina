0b33b7a595f359343f6fb2acf989c3c477bf5474
jappabr <guilherme.dion@gmail.com>
2023-08-16 19:27:14 -0300

    Update frmEstoque.Designer.cs

--- frmEstoque.Designer.cs
+++ frmEstoque.Designer.cs
@@ -30,6 +30,7 @@
         {
             dataGridView1 = new DataGridView();
             button1 = new Button();
+            textBox1 = new TextBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -38,20 +39,28 @@
             dataGridView1.BackgroundColor = Color.FromArgb(46, 51, 73);
             dataGridView1.BorderStyle = BorderStyle.None;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Location = new Point(12, 74);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(700, 400);
+            dataGridView1.Size = new Size(700, 361);
             dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
-            button1.Location = new Point(619, 442);
+            button1.Location = new Point(637, 12);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 1;
-            button1.Text = "Atualizar";
+            button1.Text = "Buscar";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(610, 23);
+            textBox1.TabIndex = 2;
             // 
             // frmEstoque
             // 
@@ -59,6 +68,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(46, 51, 73);
             ClientSize = new Size(733, 477);
+            Controls.Add(textBox1);
             Controls.Add(button1);
             Controls.Add(dataGridView1);
             FormBorderStyle = FormBorderStyle.None;
@@ -66,11 +76,13 @@
             Text = "frmEstoque";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dataGridView1;
         private Button button1;
+        private TextBox textBox1;
     }
 }
