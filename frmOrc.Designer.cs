c4ef5e29b684ccbfc373a09364bb3290b0897c51
jappabr <guilherme.dion@gmail.com>
2023-08-16 19:27:22 -0300

    Update frmOrc.Designer.cs

--- frmOrc.Designer.cs
+++ frmOrc.Designer.cs
@@ -30,28 +30,37 @@
         {
             button1 = new Button();
             dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(623, 442);
+            button1.Location = new Point(617, 13);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 3;
-            button1.Text = "Atualizar";
+            button1.Text = "Buscar";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // dataGridView1
             // 
             dataGridView1.BackgroundColor = Color.FromArgb(46, 51, 73);
             dataGridView1.BorderStyle = BorderStyle.None;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(16, 12);
+            dataGridView1.Location = new Point(12, 104);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(700, 400);
+            dataGridView1.Size = new Size(700, 332);
             dataGridView1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 13);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(599, 23);
+            textBox1.TabIndex = 4;
             // 
             // frmOrc
             // 
@@ -59,6 +68,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(46, 51, 73);
             ClientSize = new Size(733, 477);
+            Controls.Add(textBox1);
             Controls.Add(button1);
             Controls.Add(dataGridView1);
             FormBorderStyle = FormBorderStyle.None;
@@ -66,11 +76,13 @@
             Text = "frmOrc";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
         private DataGridView dataGridView1;
+        private TextBox textBox1;
     }
 }
